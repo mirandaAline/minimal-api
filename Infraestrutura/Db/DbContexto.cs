836485920defb7e0141519ad016cf5b2924e956d
Aline Costa Miranda Santos <aline.miranda@gestao.pms.ad>
2024-09-17 12:00:34 -0300

    atualização

--- Infraestrutura/Db/DbContexto.cs
+++ Infraestrutura/Db/DbContexto.cs
@@ -1,6 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using MinimalApi.Dominio.Entidades;
+
 namespace MinimalApi.Infraestutura.Db;
 
-public class DbContext
+public class DbContexto : DbContext
 {
-    
+    public DbSet<Administrador> Administradores {get; set;} = default!;
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseMySql("string de conexão",
+            ServerVersion.AutoDetect("string de conexao")
+        );
+    }
 }
